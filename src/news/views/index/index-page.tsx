161b5d86315c549a664835405787d6006649b391
Mitica <canteadumitru@gmail.com>
2018-10-26 21:46:50 +0300

    horoscope card

--- src/news/views/index/index-page.tsx
+++ src/news/views/index/index-page.tsx
@@ -4,10 +4,12 @@ import { IndexViewModel } from '../../view-models/index-view-model';
 import CommonLayout from '../common-layout';
 import { NewsLocaleNames } from '../../locale';
 import { EventListItem } from '../components/event-list-item';
+import { HoroscopeSvg } from '../../../views/components/horoscope/horoscope-svg';
+import { HoroscopeCard } from '../../../views/components/horoscope/horoscope-card';
 
 export default class IndexPage extends React.Component<IndexViewModel> {
     render() {
-        const { lang, head, translate, links, currentDate, latestEvents } = this.props;
+        const { lang, head, translate, links, currentDate, latestEvents, country } = this.props;
 
         head.elements.push(<link key='events-rss' rel="alternate" type="application/rss+xml" title={translate(NewsLocaleNames.events)} href={links.news.rss.stories({ ul: lang })}></link>);
         head.elements.push(<link key='imortant-rss' rel="alternate" type="application/rss+xml" title={translate(NewsLocaleNames.important_news)} href={links.news.rss.stories.important({ ul: lang })}></link>);
@@ -25,6 +27,7 @@ export default class IndexPage extends React.Component<IndexViewModel> {
 
         return (
             <CommonLayout {...this.props}>
+                {links.horoscope && HoroscopeSvg()}
                 <main>
                     <div className='o-layout'>
                         <div className='o-layout__item u-2/4@tablet'>
@@ -38,10 +41,10 @@ export default class IndexPage extends React.Component<IndexViewModel> {
                     </div>
                     <div className='o-layout'>
                         <div className='o-layout__item u-1/4@tablet u-1/2@mobile'>
-                            {/* <HoroscopeCard {...this.props} /> */}
+                            {links.horoscope && HoroscopeCard({links, lang, country, title:translate(NewsLocaleNames.horoscope)})}
                         </div>
                         <div className='o-layout__item u-1/4@tablet u-1/2@mobile'>
-                            {EventListItem({ root: this.props, view: 'card', item:restEvents[2] })}
+                            {EventListItem({ root: this.props, view: 'card', item: restEvents[2] })}
                         </div>
 
                         {restEvents.slice(3, 5).map(item => <div key={item.id} className='o-layout__item u-1/4@tablet u-1/2@mobile'>{EventListItem({ root: this.props, view: 'card', item })}</div>)}
---
+++ src/views/components/horoscope/horoscope-card.tsx
@@ -0,0 +1,40 @@
+
+import * as React from 'react';
+import { getHost, getSchema, Sitemap } from 'ournet.links';
+import { HoroscopesHelper, HoroscopeSign } from '@ournet/horoscopes-domain';
+import { OurnetProjectName } from '../../../ournet/data';
+
+export type HoroscopeCardProps = {
+    title: string
+    country: string
+    links: Sitemap
+    lang: string
+}
+
+export function HoroscopeCard({ title, country, links, lang }: HoroscopeCardProps) {
+
+    const host = getSchema(OurnetProjectName.HOROSCOPE, country) + '//' + getHost(OurnetProjectName.HOROSCOPE, country);
+
+    const url = host + links.horoscope.home({ ul: lang });
+
+    return (
+        <div className='c-horo-card'>
+            <a className='c-horo-card__title' title={title} href={url}>{title}</a>
+            <ul className='c-horo-card__list'>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(id => {
+                    const sign = HoroscopesHelper.getSignName(id as HoroscopeSign, lang);
+                    if (!sign) {
+                        return null;
+                    }
+                    return (
+                        <li key={id}>
+                            <a title={sign.name} href={host + links.horoscope.sign(sign.slug, { ul: lang })}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><use href={`#svg-zs-icon-${id}`}></use></svg>
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div >
+    )
+}
