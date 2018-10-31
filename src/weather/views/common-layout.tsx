b6717f8f7c43534ec4e30ffc6b0ae70a3c4ed061
Mitica <canteadumitru@gmail.com>
2018-10-31 13:18:18 +0200

    smaller event card-bare

--- src/weather/views/common-layout.tsx
+++ src/weather/views/common-layout.tsx
@@ -30,7 +30,7 @@ export default class CommonLayout extends React.Component<WeatherViewModel> {
                             <div className='c-section'>
                                 {SectionHeader({ name: translate(WeatherLocaleNames.latest_news), link: getSchema(OurnetProjectName.NEWS, country) + '//' + getHost(OurnetProjectName.NEWS, country) + links.news.home({ ul: lang }) })}
                                 <div className="o-layout o-layout--small">
-                                    {latestNews.map(item => <div key={item.id} className='o-layout__item u-1/2@tablet u-1/1@desktop'>{EventListItem({ lang, country, project, links, timezone: config.timezone, item, view: 'card-bare' })}</div>)}
+                                    {latestNews.map(item => <div key={item.id} className='o-layout__item u-1/2@tablet u-1/1@desktop'>{EventListItem({ lang, country, project, links, timezone: config.timezone, item, view: 'card-bare', imageSize: 'small' })}</div>)}
                                 </div>
                             </div>}
                         {AdAside()}
