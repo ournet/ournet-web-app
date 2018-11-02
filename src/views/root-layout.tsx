39479044e257d96865cb9731665dd14a1be8a603
Mitica <canteadumitru@gmail.com>
2018-11-02 11:10:40 +0200

    gs on body start

--- src/views/root-layout.tsx
+++ src/views/root-layout.tsx
@@ -13,8 +13,6 @@ export default class RootLayout extends React.Component<PageViewModel<IOurnetApp
             <html lang={lang}>
                 <PageHead {...this.props} />
                 <body className={`proj-${project} country-${country}`}>
-                    {children}
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                     <script dangerouslySetInnerHTML={{
                         __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -23,6 +21,8 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', '${config.googleAnalyticsId}', '${config.domain}');
 ga('set', 'dimension1', '${project}');
 ga('send', 'pageview');`}}></script>
+                    {children}
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 </body>
             </html>
         )
