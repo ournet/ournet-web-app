dd8a8e6c9379142c6b04b7cd199c2a6585a6aaa7
Mitica <canteadumitru@gmail.com>
2018-11-16 17:35:48 +0200

    weather subscribe bar

--- assets/js/weather/main.js
+++ assets/js/weather/main.js
@@ -3,3 +3,4 @@ window.$ = require('cash-dom');
 require('../news/lazy');
 require('./forecast-browser');
 require('./widget-config');
+require('./subscribe-bar');
--- src/weather/views/components/subscribe-bar.tsx
+++ src/weather/views/components/subscribe-bar.tsx
@@ -7,27 +7,26 @@ import { WeatherLocaleNames } from '../../locale';
 import { getPlaceName } from '../../../helpers';
 
 export type SubscribeBarProps = {
-    translate: TranslateFunction
-    place: Place
-    config: WeatherAppConfig
-    lang: string
+  translate: TranslateFunction
+  place: Place
+  config: WeatherAppConfig
+  lang: string
 }
 
 export function SubscribeBar({ translate, config, lang, place }: SubscribeBarProps) {
 
-    if (!config.oneSignal) {
-        return null;
-    }
+  if (!config.oneSignal) {
+    return null;
+  }
 
-    return (
-        <div>
-            <div className="c-subscribe-bar u-hidden">
-                <div className="c-subscribe-bar__btn">{translate(WeatherLocaleNames.notifications_subscribe_for_place, { name: getPlaceName(place, lang) })}</div>
-            </div>
-            <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
-            <script dangerouslySetInnerHTML={{
-                __html: `var OneSignal = window.OneSignal || [];
-
+  return (
+    <div>
+      <div className="c-subscribe-bar u-hidden" data-place-id={place.id} data-admin1-code={place.admin1Code}>
+        <div className="c-subscribe-bar__btn">{translate(WeatherLocaleNames.notifications_subscribe_for_place, { name: getPlaceName(place, lang) })}</div>
+      </div>
+      <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
+      <script dangerouslySetInnerHTML={{
+        __html: `var OneSignal = window.OneSignal || [];
 OneSignal.push(['init', {
   appId: "${config.oneSignal.appId}",
   autoRegister: false,
@@ -41,96 +40,7 @@ OneSignal.push(['init', {
     disable: true
   },
   safari_web_id: "${config.oneSignal.safari_web_id}"
-}]);
-
-OneSignal.push(function() {
-
-  var category='notifications-weather';
-  
-  function initNotifications(permission) {
-    var currentTags;
-    var placeId = '${place.id}';
-    var admin1Code = '${place.admin1Code}';
-
-    function subscribedToCurrentPlace() {
-      return currentTags && currentTags['place-id'] == placeId;
-    }
-
-    function setPlaceId() {
-      OneSignal.sendTags({'place-id': placeId, 'admin1-code': admin1Code});
-    }
-
-    function subscribeToNotifications() {
-      OneSignal.registerForPushNotifications();
-      //ga('send', 'event', category, 'show-register-native');
-    }
-
-    function hideSubscribe() {
-      $('.c-subscribe-bar').addClass('u-hidden');
-    }
-    function showSubscribe() {
-      $('.c-subscribe-bar').removeClass('u-hidden');
-    }
-
-    if(permission === 'granted') {
-      OneSignal.getTags(function(tags) {
-        currentTags = tags;
-        if (!subscribedToCurrentPlace()) {
-          showSubscribe();
-        } else {
-          if(!currentTags['admin1-code']){
-            OneSignal.sendTag('admin1-code', admin1Code);
-          }
-        }
-      });
-    } else {
-      showSubscribe();
-    }
-    
-    $('.c-subscribe-bar').click(function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      ga('send', 'event', category, 'click-subscribe-btn');
-      if(permission === 'granted') {
-        setPlaceId();
-        ga('send', 'event', category, 'changed-place-id', placeId);
-        hideSubscribe();
-      } else {
-        subscribeToNotifications();
-      }
-    });
-
-
-    OneSignal.on('notificationPermissionChange', function(permissionChange) {
-      var currentPermission = permissionChange.to;
-      if (currentPermission === 'granted') {
-        setPlaceId();
-      }
-      ga('send', 'event', category, currentPermission);
-    });
-    // Occurs when the user's subscription changes to a new value.
-    OneSignal.on('subscriptionChange', function (isSubscribed) {
-      if (isSubscribed) {
-        hideSubscribe();
-        setPlaceId();
-      } else {
-        showSubscribe();
-      }
-    });
-
-  }
-
-  // If we're on an unsupported browser, do nothing
-  if (OneSignal.isPushNotificationsSupported()) {
-    OneSignal.push(["getNotificationPermission", function(permission) {
-        initNotifications(permission);
-    }]);
-    //ga('send', 'event', category, 'supported');
-  } else {
-    hideSubscribe();
-    //ga('send', 'event', category, 'not-supported');
-  }
-});`}}></script>
-        </div>
-    )
+}]);`}}></script>
+    </div>
+  )
 }
