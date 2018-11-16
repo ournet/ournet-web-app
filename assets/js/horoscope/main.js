2ab1dc505652d1be0d6060db555c00b574491408
Mitica <canteadumitru@gmail.com>
2018-11-16 16:59:49 +0200

    horo subscribe bar script

--- assets/js/horoscope/main.js
+++ assets/js/horoscope/main.js
@@ -1,3 +1,4 @@
 
 window.$ = require('cash-dom');
 require('../news/lazy');
+require('./subscribe-bar');
--- src/horoscope/views/sign/sign-page.tsx
+++ src/horoscope/views/sign/sign-page.tsx
@@ -33,10 +33,10 @@ export function SignPage(props: SignViewModel) {
             </main>
             {config.oneSignal &&
                 <div>
+                    <div className='c-subscribe-bar u-hidden' data-sign-id={report.sign}></div>
                     <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
                     <script dangerouslySetInnerHTML={{
                         __html: `var OneSignal = window.OneSignal || [];
-
 OneSignal.push(['init', {
     appId: "${config.oneSignal.appId}",
     autoRegister: false,
@@ -50,92 +50,7 @@ OneSignal.push(['init', {
     disable: true
     },
     safari_web_id: "${config.oneSignal.safari_web_id}"
-}]);
-
-OneSignal.push(function() {
-
-    var category='notifications-horo';
-    
-    function initNotifications(permission) {
-        var currentTags;
-        var signId = '${report.sign}';
-
-    function subscribedToCurrentSign() {
-        return currentTags && currentTags['zodiac-sign'] == signId;
-    }
-
-    function setTags() {
-        OneSignal.sendTags({'zodiac-sign': signId});
-    }
-
-    function subscribeToNotifications() {
-        OneSignal.registerForPushNotifications();
-        ga('send', 'event', category, 'show-register-native');
-    }
-
-    function hideSubscribe() {
-        $('.c-subscribe-bar').addClass('hidden');
-    }
-    function showSubscribe() {
-        $('.c-subscribe-bar').removeClass('hidden');
-    }
-
-    if(permission === 'granted') {
-        OneSignal.getTags(function(tags) {
-        currentTags = tags;
-        if (!subscribedToCurrentSign()) {
-            showSubscribe();
-        }
-        });
-    } else {
-        showSubscribe();
-        setTimeout(subscribeToNotifications, 1000*5);
-    }
-    
-    $('.c-subscribe-bar').click(function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        ga('send', 'event', category, 'click-subscribe-btn');
-        if(permission === 'granted') {
-            setTags();
-            ga('send', 'event', category, 'changed-sign', signId);
-            hideSubscribe();
-        } else {
-            subscribeToNotifications();
-        }
-    });
-
-
-    OneSignal.on('notificationPermissionChange', function(permissionChange) {
-        var currentPermission = permissionChange.to;
-        if (currentPermission === 'granted') {
-            setTags();
-        }
-        ga('send', 'event', category, currentPermission);
-    });
-    // Occurs when the user's subscription changes to a new value.
-    OneSignal.on('subscriptionChange', function (isSubscribed) {
-        if (isSubscribed) {
-            hideSubscribe();
-            setTags();
-        } else {
-            showSubscribe();
-        }
-    });
-
-    }
-
-    // If we're on an unsupported browser, do nothing
-    if (OneSignal.isPushNotificationsSupported()) {
-        OneSignal.push(["getNotificationPermission", function(permission) {
-            initNotifications(permission);
-        }]);
-        //ga('send', 'event', category, 'supported');
-    } else {
-        hideSubscribe();
-        //ga('send', 'event', category, 'not-supported');
-    }
-});`}}></script>
+}]);`}}></script>
                 </div>}
             {config.facebookId && FacebookScript(config.facebookId, lang, country)}
         </CommonLayout>
