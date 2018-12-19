64e8b5089917b202616b47fe807300925fbaa9be
Mitica <canteadumitru@gmail.com>
2018-12-19 16:29:32 +0200

    no log 404

--- src/ournet/app.ts
+++ src/ournet/app.ts
@@ -4,6 +4,7 @@ import { App } from "../base/app";
 import { Request } from "../base/types";
 import logger from "../logger";
 import env from "../env";
+import { isBoom } from "boom";
 
 export abstract class OurnetApp<DATA extends OurnetAppData> extends App<DATA> {
     constructor(routes: IRouter[], protected readonly project: OurnetProjectName) {
@@ -11,6 +12,12 @@ export abstract class OurnetApp<DATA extends OurnetAppData> extends App<DATA> {
     }
 
     protected logError(error: Error, req: Request) {
+        if (isBoom(error)) {
+            const err = <any>error;
+            if (err.output && err.output.statusCode === 404) {
+                return;
+            }
+        }
         logger.error(error.message, { url: req.url, ...error });
         if (!env.isProduction) {
             console.trace(error);
