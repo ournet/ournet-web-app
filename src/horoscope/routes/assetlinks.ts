5f9d965266a341f0f7c03896f64fb2a2e7aaf51c
Mitica <canteadumitru@gmail.com>
2019-01-07 20:17:39 +0200

    fix assetlinks

--- src/horoscope/routes/assetlinks.ts
+++ src/horoscope/routes/assetlinks.ts
@@ -13,16 +13,13 @@ export class AssetlinksRouter extends OurnetRouter {
             code: 200,
             req,
             res,
-            data: [
-                {
-                    relation: ['delegate_permission/common.handle_all_urls'],
-                    target: {
-                        namespane: 'android_app',
-                        package_name: 'com.ournet.horoscope',
-                        sha256_cert_fingerprinters: ['63:69:E2:8C:C4:5E:BE:EB:BE:98:3E:CB:FE:C6:D5:CC:8D:69:A8:E4:36:D9:B3:9E:16:AD:E8:9B:17:35:46:76'],
-                    }
+            data: [{
+                "relation": ["delegate_permission/common.handle_all_urls"],
+                "target": {
+                    "namespace": "android_app", "package_name": "com.ournet.horoscope",
+                    "sha256_cert_fingerprints": ["63:69:E2:8C:C4:5E:BE:EB:BE:98:3E:CB:FE:C6:D5:CC:8D:69:A8:E4:36:D9:B3:9E:16:AD:E8:9B:17:35:46:76"]
                 }
-            ],
+            }],
             headers: {
                 'content-type': 'application/json',
             },
