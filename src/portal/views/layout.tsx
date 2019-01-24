034e8f398aa5af6abe10506b0506aec328ed325a
Mitica <canteadumitru@gmail.com>
2019-01-24 12:48:19 +0200

    portal rambler top100 script

--- src/portal/views/layout.tsx
+++ src/portal/views/layout.tsx
@@ -8,7 +8,7 @@ import { PageFooter } from './components/page-footer';
 
 export default class Layout extends React.Component<PortalViewModel, any> {
     render() {
-        const { children } = this.props;
+        const { children, country } = this.props;
 
         return (
             <RootLayout {...this.props}>
@@ -18,7 +18,43 @@ export default class Layout extends React.Component<PortalViewModel, any> {
                     {children}
                 </div>
                 {PageFooter(this.props)}
+                {getFooterScripts(country)}
             </RootLayout>
         )
     }
+}
+
+function getFooterScripts(country: string) {
+    if (country === 'ru') {
+        return (
+            <ins>
+                <script dangerouslySetInnerHTML={{
+                    __html: `(function (w, d, c) {
+                    (w[c] = w[c] || []).push(function() {
+                        var options = {
+                            project: 2677032,
+                        };
+                        try {
+                            w.top100Counter = new top100(options);
+                        } catch(e) { }
+                    });
+                    var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src =
+                    (d.location.protocol == "https:" ? "https:" : "http:") +
+                    "//st.top100.ru/top100/top100.js";
+
+                    if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+                })(window, document, "_top100q");`}}>
+                </script>
+            </ins>
+        )
+    }
+
+    return null;
 }
