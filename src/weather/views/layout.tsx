2ec681b3abec8a5016d4293c1acb0f4882b6ee08
Mitica <canteadumitru@gmail.com>
2019-04-03 09:40:07 +0300

    meteo2.kz rambler counter

--- src/weather/views/layout.tsx
+++ src/weather/views/layout.tsx
@@ -96,6 +96,36 @@ function getFooterScripts(country: string) {
             </ins>
         )
     }
+    if (country === 'kz') {
+        return (
+            <ins>
+                <script dangerouslySetInnerHTML={{
+                    __html: `(function (w, d, c) {
+                    (w[c] = w[c] || []).push(function() {
+                        var options = {
+                            project: 6593194,
+                        };
+                        try {
+                            w.top100Counter = new top100(options);
+                        } catch(e) { }
+                    });
+                    var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src =
+                    (d.location.protocol == "https:" ? "https:" : "http:") +
+                    "//st.top100.ru/top100/top100.js";
+
+                    if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+                })(window, document, "_top100q");`}}>
+                </script>
+            </ins>
+        )
+    }
 
     return null;
 }
