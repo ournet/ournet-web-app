f1ba276061145f3a8d73f3ec3e9049637f95c7d1
Mitica <canteadumitru@gmail.com>
2019-06-07 18:26:31 +0300

    news index layout

--- src/news/views/index/index-page.tsx
+++ src/news/views/index/index-page.tsx
@@ -10,54 +10,32 @@ import PageContentSection from '../../../views/components/page-content-section';
 
 export default class IndexPage extends React.Component<IndexViewModel> {
     render() {
-        const { lang, head, locales, links, currentDate, latestEvents, country, latestQuotes, config } = this.props;
+        const { lang, head, locales, links, latestEvents, country, latestQuotes, config } = this.props;
 
         head.elements.push(<link key='events-rss' rel="alternate" type="application/rss+xml" title={locales.events()} href={links.news.rss.stories({ ul: lang })}></link>);
         head.elements.push(<link key='imortant-rss' rel="alternate" type="application/rss+xml" title={locales.important_news()} href={links.news.rss.stories.important({ ul: lang })}></link>);
-
-        const recentDate = currentDate.clone().add(-12, 'hours').toISOString();
-
-        let recentEvents = latestEvents.filter(item => item.createdAt > recentDate);
-
-        if (!recentEvents.length) {
-            recentEvents = latestEvents;
-        }
-
-        const mainEvent = recentEvents.sort((a, b) => b.countNews - a.countNews)[0];
-        const restEvents = latestEvents.filter(item => item.id !== mainEvent.id);
 
         return (
             <CommonLayout {...this.props}>
                 <main>
                     <PageContentSection>
                         <div className='o-layout'>
-                            <div className='o-layout__item u-2/4@tablet'>
-                                {mainEvent && EventListItem({ lang, country, links, timezone: config.timezone, imageSize: 'large', view: 'card-wide', item: mainEvent })}
-                            </div>
-                            <div className='o-layout__item u-2/4@tablet'>
-                                <div className='o-layout'>
-                                    {restEvents.slice(0, 2).map(item => <div key={item.id} className='o-layout__item u-1/2@mobile'>{EventListItem({ lang, country, item, links, timezone: config.timezone, view: 'card' })}</div>)}
-                                </div>
-                            </div>
+                            {latestEvents.slice(0, 4).map(item => <div key={item.id} className='o-layout__item u-1/4@tablet u-1/2@mobile'>{EventListItem({ lang, country, item, links, timezone: config.timezone, view: 'card' })}</div>)}
                         </div>
                         <div className='o-layout'>
                             <div className='o-layout__item u-1/4@tablet u-1/2@mobile'>
                                 {links.horoscope && HoroscopeCard({ links, lang, country, title: locales.horoscope() })}
                             </div>
-                            <div className='o-layout__item u-1/4@tablet u-1/2@mobile'>
-                                {restEvents[2] && EventListItem({ lang, country, links, timezone: config.timezone, view: 'card', item: restEvents[2] })}
-                            </div>
-
-                            {restEvents.slice(3, 5).map(item => <div key={item.id} className='o-layout__item u-1/4@tablet u-1/2@mobile'>{EventListItem({ lang, country, item, links, timezone: config.timezone, view: 'card' })}</div>)}
+                            {latestEvents.slice(4, 7).map(item => <div key={item.id} className='o-layout__item u-1/4@tablet u-1/2@mobile'>{EventListItem({ lang, country, item, links, timezone: config.timezone, view: 'card' })}</div>)}
                         </div>
                         <div className='c-group'>
                             {GroupHeader({ name: locales.latest_quotes(), link: links.news.quotes({ ul: lang }), type: 'important' })}
                             <div className='o-layout'>
-                                {latestQuotes.map(item => <div key={item.id} className='o-layout__item u-1/3@tablet'>{QuoteListItem({ lang, country, links, timezone: config.timezone, view: 'card', item })}</div>)}
+                                {latestQuotes && latestQuotes.map(item => <div key={item.id} className='o-layout__item u-1/3@tablet'>{QuoteListItem({ lang, country, links, timezone: config.timezone, view: 'card', item })}</div>)}
                             </div>
                         </div>
                         <div className='o-layout'>
-                            {restEvents.slice(5).map(item => <div key={item.id} className='o-layout__item u-1/2@mobile u-1/4@tablet'>{EventListItem({ lang, country, item, links, timezone: config.timezone, view: 'card' })}</div>)}
+                            {latestEvents.slice(7).map(item => <div key={item.id} className='o-layout__item u-1/2@mobile u-1/4@tablet'>{EventListItem({ lang, country, item, links, timezone: config.timezone, view: 'card' })}</div>)}
                         </div>
                     </PageContentSection>
                 </main>
