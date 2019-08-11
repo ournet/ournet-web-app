bd767c6cd86cb5cf9979f9310043d34f44d8924d
Mitica <canteadumitru@gmail.com>
2019-08-12 00:37:44 +0300

    moti2 right ad

--- src/weather/views/common-layout.tsx
+++ src/weather/views/common-layout.tsx
@@ -39,6 +39,14 @@ export default class CommonLayout extends React.Component<WeatherViewModel> {
                         </div>
                         <div className="o-layout__item u-2/6@tablet">
                             {!config.disabledAds && AdAside()}
+                            {config.domain === "moti2.al" && <div id="M454705ScriptRootC701836"><div id="M454705PreloadC701836"></div><script dangerouslySetInnerHTML={{
+                                __html: `(function(){
+                                    var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
+                                    var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M454705ScriptRootC701836")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
+                                    catch(e){var iw=d;var c=d[gi]("M454705ScriptRootC701836");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=701836;c[ac](dv);
+                                    var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/m/o/moti2.al.701836.js?t="+D.getUTCFullYear()+D.getUTCMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();`}}>
+                            </script></div>
+                            }
                             {latestNews && latestNews.length > 0 &&
                                 <div className='c-section'>
                                     {SectionHeader({ name: locales.latest_news(), link: getSchema(OurnetProjectName.NEWS, country) + '//' + getHost(OurnetProjectName.NEWS, country) + links.news.home({ ul: lang }) })}
