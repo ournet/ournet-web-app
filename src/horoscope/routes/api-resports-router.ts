9d6795164afe8cb0acf91d806179d7a6bffeb7f5
Dumitru C <canteadumitru@gmail.com>
2020-08-30 11:34:13 +0300

    api reports 12h cache

--- src/horoscope/routes/api-resports-router.ts
+++ src/horoscope/routes/api-resports-router.ts
@@ -2,82 +2,96 @@ import { HoroscopeBaseRouter } from "../router";
 import { Request, Response } from "../../base/types";
 import { HoroscopeBaseHandler } from "../handlers/handler";
 import { HoroscopeAppData } from "../data";
-import { HoroscopeViewModelBuilder, HoroscopeViewModel } from "../view-models/horoscope-view-model";
+import {
+  HoroscopeViewModelBuilder,
+  HoroscopeViewModel
+} from "../view-models/horoscope-view-model";
 import { HoroscopeReport } from "@ournet/api-client";
 import { OurnetViewModelInput } from "../../ournet/view-model";
 import { HoroscopesHelper, HoroscopeSign } from "@ournet/horoscopes-domain";
 import logger from "../../logger";
 
 export class ApiReportsRouter extends HoroscopeBaseRouter {
-    constructor() {
-        super('/api/reports.json');
-    }
-    protected createHander(req: Request, res: Response) {
-        const input = this.formatInput<ApiReportsViewModelInput>(req, res);
-        input.period = input.url.query.period as string;
-        input.client = input.url.query.client as string;
-        return new ApiReportsHandler(input);
-    }
+  constructor() {
+    super("/api/reports.json");
+  }
+  protected createHander(req: Request, res: Response) {
+    const input = this.formatInput<ApiReportsViewModelInput>(req, res);
+    input.period = input.url.query.period as string;
+    input.client = input.url.query.client as string;
+    return new ApiReportsHandler(input);
+  }
 }
 
 class ApiReportsHandler extends HoroscopeBaseHandler<ApiReportsViewModelInput> {
-    async handle(appData: HoroscopeAppData) {
-
-        if (!this.input.client) {
-            return this.send({ message: `Param 'client' is required!` }, 400);
-        }
-
-        logger.warn(`api reports ${this.input.client}`);
-
-        let period = this.input.period;
-
-        if (period && !/^[WD]\d+$/.test(period)) {
-            return this.send({ message: `Param 'period' is invalid. Example: D20181018` }, 400);
-        }
-
-        const model = await new ApiReportsViewModelBuilder(this.input, appData).build();
-
-        period = period || model.currentDayPeriod;
-
-
-        this.setCacheControl(60 * 24 * 30);
-
-        return this.send({ data: { period, reports: model.reports || [] } }, 200);
+  async handle(appData: HoroscopeAppData) {
+    if (!this.input.client) {
+      return this.send({ message: `Param 'client' is required!` }, 400);
     }
+
+    logger.warn(`api reports ${this.input.client}`);
+
+    let period = this.input.period;
+
+    if (period && !/^[WD]\d+$/.test(period)) {
+      return this.send(
+        { message: `Param 'period' is invalid. Example: D20181018` },
+        400
+      );
+    }
+
+    const model = await new ApiReportsViewModelBuilder(
+      this.input,
+      appData
+    ).build();
+
+    period = period || model.currentDayPeriod;
+
+    this.setCacheControl(model.reports.length > 0 ? 60 * 12 : 1);
+
+    return this.send({ data: { period, reports: model.reports || [] } }, 200);
+  }
 }
 
-class ApiReportsViewModelBuilder
-    extends HoroscopeViewModelBuilder<ApiReportsViewModel, ApiReportsViewModelInput> {
+class ApiReportsViewModelBuilder extends HoroscopeViewModelBuilder<
+  ApiReportsViewModel,
+  ApiReportsViewModelInput
+> {
+  build() {
+    const { lang, currentDayPeriod } = this.model;
 
-    build() {
+    const period = this.input.period || currentDayPeriod;
 
-        const { lang, currentDayPeriod } = this.model;
+    const reportLang = (this.input.url.query.lang as string) || lang;
 
-        const period = this.input.period || currentDayPeriod;
+    const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((sign) =>
+      HoroscopesHelper.createReportId(period, reportLang, sign as HoroscopeSign)
+    );
 
-        const reportLang = this.input.url.query.lang as string || lang;
+    this.apiClient.horoscopesReportsByIds(
+      "reports",
+      {
+        fields: "id lang sign text period numbers stats {love health success}"
+      },
+      { ids }
+    );
 
-        const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-            .map(sign => HoroscopesHelper.createReportId(period, reportLang, sign as HoroscopeSign));
+    return super.build();
+  }
 
-        this.apiClient.horoscopesReportsByIds('reports', { fields: 'id lang sign text period numbers stats {love health success}' }, { ids });
+  protected formatModelData(data: ApiReportsViewModel) {
+    const model = super.formatModelData(data);
+    model.reports = (model.reports || []).sort((a, b) => a.sign - b.sign);
 
-        return super.build();
-    }
-
-    protected formatModelData(data: ApiReportsViewModel) {
-        const model = super.formatModelData(data);
-        model.reports = (model.reports || []).sort((a, b) => a.sign - b.sign);
-
-        return model;
-    }
+    return model;
+  }
 }
 
 interface ApiReportsViewModelInput extends OurnetViewModelInput {
-    period?: string
-    client: string
+  period?: string;
+  client: string;
 }
 
 interface ApiReportsViewModel extends HoroscopeViewModel {
-    reports: HoroscopeReport[]
+  reports: HoroscopeReport[];
 }
