6aedf9e4f4bc54f2b3819c46d2aea9aaae567155
Dumitru C <canteadumitru@gmail.com>
2021-11-28 16:18:57 +0200

    Weather banner

--- src/weather/views/components/forecast/place-daily-report.tsx
+++ src/weather/views/components/forecast/place-daily-report.tsx
@@ -1,83 +1,122 @@
-import * as React from 'react';
-import { Place, HoursForecastDataBlock, HoursForecastDataPoint, PublicHoliday } from '@ournet/api-client';
-import * as moment from 'moment-timezone';
-import { WeatherAppConfig } from '../../../config';
-import { PlaceDayReport } from './place-day-report';
-import { AdTop } from '../ads/ad-top';
-import { SubscribeBar } from '../subscribe-bar';
-import { AdCenter } from '../ads/ad-center';
-import { OurnetLocales } from '../../../../locales';
+import * as React from "react";
+import {
+  Place,
+  HoursForecastDataBlock,
+  HoursForecastDataPoint,
+  PublicHoliday
+} from "@ournet/api-client";
+import * as moment from "moment-timezone";
+import { WeatherAppConfig } from "../../../config";
+import { PlaceDayReport } from "./place-day-report";
+import { AdTop } from "../ads/ad-top";
+import { SubscribeBar } from "../subscribe-bar";
+import { AdCenter } from "../ads/ad-center";
+import { OurnetLocales } from "../../../../locales";
 
 export type PlaceDailyReportPorps = {
-    lang: string
-    locales: OurnetLocales
-    config: WeatherAppConfig
-    place: Place
-    report?: HoursForecastDataBlock
-    holidays: PublicHoliday[]
-}
+  lang: string;
+  locales: OurnetLocales;
+  config: WeatherAppConfig;
+  place: Place;
+  report?: HoursForecastDataBlock;
+  holidays: PublicHoliday[];
+};
 
-export function PlaceDailyReport({ place, report, locales, lang, config, holidays }: PlaceDailyReportPorps) {
+export function PlaceDailyReport({
+  place,
+  report,
+  locales,
+  lang,
+  config,
+  holidays
+}: PlaceDailyReportPorps) {
+  const daysData: HoursForecastDataPoint[][] = [];
 
-    const daysData: HoursForecastDataPoint[][] = []
+  if (!report || !report.data) {
+    return <div className="c-nodata">{locales.forecast_no_data()}</div>;
+  }
 
+  const timezone = !!moment.tz.zone(place.timezone)
+    ? place.timezone
+    : config.timezone;
+  let lastDay = -1;
 
-    if (!report || !report.data) {
-        return <div className='c-nodata'>{locales.forecast_no_data()}</div>
+  report.data.forEach((item) => {
+    let list = daysData[daysData.length - 1] || [];
+    const date = moment(item.time * 1000).tz(timezone);
+    if (lastDay !== date.date()) {
+      list = [];
+      daysData.push(list);
     }
+    list.push(item);
+    lastDay = date.date();
+  });
 
-    const timezone = !!moment.tz.zone(place.timezone) ? place.timezone : config.timezone;
-    let lastDay = -1;
+  const currentUnixTime = moment().tz(timezone).unix();
+  const firstDayData = Array.from(daysData[0]);
+  const lastDayDateUnixTime = moment(
+    firstDayData[firstDayData.length - 1].time * 1000
+  )
+    .tz(timezone)
+    .unix();
+  if (currentUnixTime > lastDayDateUnixTime) {
+    daysData.shift();
+  }
 
-    report.data.forEach(item => {
-        let list = daysData[daysData.length - 1] || [];
-        const date = moment(item.time * 1000).tz(timezone);
-        if (lastDay !== date.date()) {
-            list = [];
-            daysData.push(list);
-        }
-        list.push(item);
-        lastDay = date.date();
-    });
+  const items: (JSX.Element | null)[] = [];
 
-    const currentUnixTime = moment().tz(timezone).unix();
-    const firstDayData = Array.from(daysData[0]);
-    const lastDayDateUnixTime = moment(firstDayData[firstDayData.length - 1].time * 1000).tz(timezone).unix();
-    if (currentUnixTime > lastDayDateUnixTime) {
-        daysData.shift();
-    }
-
-    const items: (JSX.Element | null)[] = []
-
-    daysData.forEach((dayData, index) => {
-        if (index === 1) {
-            if (!config.disabledAds) {
-                items.push(AdTop())
-            }
-            if (config.domain === "moti2.al") {
-                items.push(<div id="M454705ScriptRootC701831"><div id="M454705PreloadC701831"></div><script dangerouslySetInnerHTML={{
-                    __html: `(function(){
+  daysData.forEach((dayData, index) => {
+    if (index === 1) {
+      if (!config.disabledAds) {
+        items.push(AdTop());
+      }
+      if (config.domain === "moti2.al") {
+        items.push(
+          <div id="M454705ScriptRootC701831">
+            <div id="M454705PreloadC701831"></div>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(){
                         var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
                         var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M454705ScriptRootC701831")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
                         catch(e){var iw=d;var c=d[gi]("M454705ScriptRootC701831");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=701831;c[ac](dv);
-                        var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/m/o/moti2.al.701831.js?t="+D.getUTCFullYear()+D.getUTCMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();`}}>
-                </script></div>);
-            }
-        }
-        else if (index === 6) {
-            items.push(SubscribeBar({ locales, config, lang, place }))
-        }
-        else if (index === 5) {
-            if (!config.disabledAds) {
-                items.push(AdCenter())
-            }
-        }
-        items.push(PlaceDayReport({ lang, locales, holidays, filter: index === 0, config, place, report: { icon: 0, data: dayData } }));
-    })
-
-    return (
-        <div className='c-daily-report'>
-            {items}
+                        var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/m/o/moti2.al.701831.js?t="+D.getUTCFullYear()+D.getUTCMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();`
+              }}
+            ></script>
+          </div>
+        );
+      }
+    } else if (index === 6) {
+      items.push(SubscribeBar({ locales, config, lang, place }));
+    } else if (index === 5) {
+      if (!config.disabledAds) {
+        items.push(AdCenter());
+      }
+    } else if (index === 8) {
+      items.push(
+        <div className="ad-maiar_full">
+          <a
+            title="Maiar Wallet"
+            target="_blank"
+            rel="nofollow"
+            className="ad-maiar"
+            href="https://get.maiar.com/referral/72yoz0afwu"
+          ></a>
         </div>
-    )
+      );
+    }
+    items.push(
+      PlaceDayReport({
+        lang,
+        locales,
+        holidays,
+        filter: index === 0,
+        config,
+        place,
+        report: { icon: 0, data: dayData }
+      })
+    );
+  });
+
+  return <div className="c-daily-report">{items}</div>;
 }
