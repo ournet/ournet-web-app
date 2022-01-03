098b2715f17c00bc5ef277be318efa1a87216e62
Dumitru C <canteadumitru@gmail.com>
2022-01-03 16:45:49 +0200

    remove ournet.logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,24 +1,11 @@
-
-const logger = require('ournet.logger');
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-if (isProduction) {
-    logger.loggly({
-		tags: ['ournet-web-app'],
-		json: true
-	});
-	logger.removeConsole();
-}
-
 export default {
-    info(...meta: any[]) {
-        logger.info(meta);
-    },
-    warn(...meta: any[]) {
-        logger.warn(meta);
-    },
-    error(...meta: any[]) {
-        logger.error(meta);
-    },
-}
+  info(...meta: any[]) {
+    console.info(meta);
+  },
+  warn(...meta: any[]) {
+    console.warn(meta);
+  },
+  error(...meta: any[]) {
+    console.error(meta);
+  }
+};
