863c9a25050f0fcc1e8fa5a1db5b0bc93ca5a001
Dumitru C <canteadumitru@gmail.com>
2022-01-03 12:23:08 +0200

    country names fix

--- src/locales.ts
+++ src/locales.ts
@@ -1,61 +1,73 @@
-
-import { LocalesKey, LocalizyLocalesProvider, LocalizyLocales } from './generated-locales';
-import { parseTranslationData, Locales } from 'localizy';
-import { join, basename } from 'path';
-import { OurnetProjectName } from './ournet/data';
-import { readdirSync, readFileSync } from 'fs';
+import {
+  LocalesKey,
+  LocalizyLocalesProvider,
+  LocalizyLocales
+} from "./generated-locales";
+import { parseTranslationData, Locales } from "localizy";
+import { join, basename } from "path";
+import { OurnetProjectName } from "./ournet/data";
+import { readdirSync, readFileSync } from "fs";
+const countryNames = JSON.parse(
+  readFileSync(join(__dirname, "../data/country-names.json"), "utf8")
+);
 
 export type Locale = {
-    lang: string
-    country: string
-}
+  lang: string;
+  country: string;
+};
 
 export class OurnetTranslator extends LocalizyLocalesProvider<OurnetLocales> {
-    protected createInstance(t: Locales) {
-        return new OurnetLocales(t);
-    }
+  protected createInstance(t: Locales) {
+    return new OurnetLocales(t);
+  }
 }
 
 export const OURNET_TRANSLATOR = new OurnetTranslator({
-    data: readdirSync(join(__dirname, '..', 'locales'))
-        .filter(file => file.endsWith('.json'))
-        .map(file => ({
-            lang: basename(file, '.json'),
-            data: parseTranslationData(JSON.parse(readFileSync(join(__dirname, '..', 'locales', file), 'utf8'))),
-        }))
-        .reduce<any>((dic, item) => {
-            dic[item.lang] = item.data;
-            return dic;
-        }, {}),
-    throwUndefinedKey: true,
-})
+  data: readdirSync(join(__dirname, "..", "locales"))
+    .filter((file) => file.endsWith(".json"))
+    .map((file) => ({
+      lang: basename(file, ".json"),
+      data: parseTranslationData(
+        JSON.parse(readFileSync(join(__dirname, "..", "locales", file), "utf8"))
+      )
+    }))
+    .reduce<any>((dic, item) => {
+      dic[item.lang] = item.data;
+      return dic;
+    }, {}),
+  throwUndefinedKey: true
+});
 
 export class OurnetLocales extends LocalizyLocales {
-    getCountryName(countryCode: string) {
-        return this.s(`country_${countryCode}` as LocalesKey);
-    }
+  getCountryName(countryCode: string) {
+    return (
+      (countryNames[countryCode.toLocaleLowerCase()] || {})[
+        this.__locales.language()
+      ] || ""
+    );
+  }
 
-    getInCountryName(countryCode: string) {
-        return this.s(`in_country_${countryCode}` as LocalesKey);
-    }
+  getInCountryName(countryCode: string) {
+    return this.s(`in_country_${countryCode}` as LocalesKey);
+  }
 
-    getLanguageName(languageCode: string) {
-        return this.s(`language_${languageCode}` as LocalesKey);
-    }
+  getLanguageName(languageCode: string) {
+    return this.s(`language_${languageCode}` as LocalesKey);
+  }
 
-    getProjectName(project: OurnetProjectName) {
-        return this.s(project as LocalesKey);
-    }
+  getProjectName(project: OurnetProjectName) {
+    return this.s(project as LocalesKey);
+  }
 
-    getAppName(project: OurnetProjectName, country: string) {
-        return this.s(`${project}_${country}_app_name` as LocalesKey);
-    }
+  getAppName(project: OurnetProjectName, country: string) {
+    return this.s(`${project}_${country}_app_name` as LocalesKey);
+  }
 
-    getShortAppName(project: OurnetProjectName, country: string) {
-        return this.s(`${project}_${country}_short_app_name` as LocalesKey);
-    }
+  getShortAppName(project: OurnetProjectName, country: string) {
+    return this.s(`${project}_${country}_short_app_name` as LocalesKey);
+  }
 
-    getHoroStatItemName(item: 'health' | 'love' | 'success') {
-        return this.s(item as LocalesKey);
-    }
+  getHoroStatItemName(item: "health" | "love" | "success") {
+    return this.s(item as LocalesKey);
+  }
 }
--- src/views/components/page-footer.tsx
+++ src/views/components/page-footer.tsx
@@ -1,43 +1,79 @@
-
-import * as React from 'react';
-import { getSchema, getHost } from 'ournet.links';
-import { OurnetProjectName } from '../../ournet/data';
-import { OurnetLocales } from '../../locales';
-import { OurnetAppConfig } from '../../ournet/config';
-import { PageHeadViewData } from '../../ournet/page-view-model';
+import * as React from "react";
+import { getSchema, getHost } from "ournet.links";
+import { OurnetProjectName } from "../../ournet/data";
+import { OurnetLocales } from "../../locales";
+import { OurnetAppConfig } from "../../ournet/config";
+import { PageHeadViewData } from "../../ournet/page-view-model";
 
 interface PageFooterProps {
-    project: OurnetProjectName
-    locales: OurnetLocales
-    lang: string
-    config: OurnetAppConfig
-    country: string
-    version: string
-    head: PageHeadViewData
-    utilLinks?: { id: string, url: string, text: string, title?: string }[]
-    preInfo?: React.ReactNode
+  project: OurnetProjectName;
+  locales: OurnetLocales;
+  lang: string;
+  config: OurnetAppConfig;
+  country: string;
+  version: string;
+  head: PageHeadViewData;
+  utilLinks?: { id: string; url: string; text: string; title?: string }[];
+  preInfo?: React.ReactNode;
 }
 
-export function PageFooter({ project, locales, config, country, version, utilLinks, preInfo }: PageFooterProps) {
-
-    return (
-        <footer className='c-footer'>
-            <div className='o-wrapper'>
-                <div className='o-layout'>
-                    <div className='o-layout__item u-1/3@tablet o-footer-info'>
-                        <div>{locales.contact()} <a href={'mailto:' + config.email}>{config.email}</a></div>
-                        {preInfo}
-                        <p>Version: {version}</p>
-                    </div>
-                    <div className='o-layout__item u-1/3@tablet o-footer-useful'>
-                        {utilLinks && utilLinks.map(item => <div key={item.id}><a href={item.url} title={item.title}>{item.text}</a></div>)}
-                        {config.projects.map(item => <div key={item}><a href={getSchema(item, country) + '//' + getHost(item, country)}>{locales.getProjectName(item)}</a></div>)}
-                    </div>
-                    <div className='o-layout__item u-1/3@tablet o-footer-sites'>
-                        {config.internationalIds.map(code => <div key={code}><a href={getSchema(project, code) + '//' + getHost(project, code)}>{locales.getCountryName(code)}</a></div>)}
-                    </div>
-                </div>
+export function PageFooter({
+  project,
+  locales,
+  config,
+  country,
+  version,
+  utilLinks,
+  preInfo
+}: PageFooterProps) {
+  return (
+    <footer className="c-footer">
+      <div className="o-wrapper">
+        <div className="o-layout">
+          <div className="o-layout__item u-1/3@tablet o-footer-info">
+            <div>
+              {locales.contact()}{" "}
+              <a href={"mailto:" + config.email}>{config.email}</a>
             </div>
-        </footer>
-    )
+            {preInfo}
+            <p>Version: {version}</p>
+          </div>
+          <div className="o-layout__item u-1/3@tablet o-footer-useful">
+            {utilLinks &&
+              utilLinks.map((item) => (
+                <div key={item.id}>
+                  <a href={item.url} title={item.title}>
+                    {item.text}
+                  </a>
+                </div>
+              ))}
+            {config.projects.map((item) => (
+              <div key={item}>
+                <a
+                  href={
+                    getSchema(item, country) + "//" + getHost(item, country)
+                  }
+                >
+                  {locales.getProjectName(item)}
+                </a>
+              </div>
+            ))}
+          </div>
+          <div className="o-layout__item u-1/3@tablet o-footer-sites">
+            {config.internationalIds.map((code) => (
+              <div key={code}>
+                <a
+                  href={
+                    getSchema(project, code) + "//" + getHost(project, code)
+                  }
+                >
+                  {locales.getCountryName(code)}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
