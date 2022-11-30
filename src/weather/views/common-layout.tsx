bbb266515f6a64d711cadba2d87003cc5c577363
Dumitru C <canteadumitru@gmail.com>
2022-11-30 12:22:44 +0200

    update mgid scripts

--- src/weather/views/common-layout.tsx
+++ src/weather/views/common-layout.tsx
@@ -51,16 +51,11 @@ export default class CommonLayout extends React.Component<WeatherViewModel> {
             <div className="o-layout__item u-2/6@tablet">
               {!config.disabledAds && AdAside()}
               {config.domain === "moti2.al" && (
-                <div id="M454705ScriptRootC701836">
-                  <div id="M454705PreloadC701836"></div>
+                <div>
+                  <div id="M454705ScriptRootC701836"></div>
                   <script
-                    dangerouslySetInnerHTML={{
-                      __html: `(function(){
-                                    var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
-                                    var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M454705ScriptRootC701836")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
-                                    catch(e){var iw=d;var c=d[gi]("M454705ScriptRootC701836");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=701836;c[ac](dv);
-                                    var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/m/o/moti2.al.701836.js?t="+D.getUTCFullYear()+D.getUTCMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();`
-                    }}
+                    src="https://jsc.mgid.com/m/o/moti2.al.701836.js"
+                    async
                   ></script>
                 </div>
               )}
--- src/weather/views/components/forecast/place-daily-report.tsx
+++ src/weather/views/components/forecast/place-daily-report.tsx
@@ -71,16 +71,11 @@ export function PlaceDailyReport({
       }
       if (config.domain === "moti2.al") {
         items.push(
-          <div key="moti2-div" id="M454705ScriptRootC701831">
-            <div id="M454705PreloadC701831"></div>
+          <div key="moti2-div">
+            <div id="M454705ScriptRootC701831"></div>
             <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(){
-                        var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
-                        var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M454705ScriptRootC701831")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
-                        catch(e){var iw=d;var c=d[gi]("M454705ScriptRootC701831");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=701831;c[ac](dv);
-                        var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/m/o/moti2.al.701831.js?t="+D.getUTCFullYear()+D.getUTCMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();`
-              }}
+              src="https://jsc.mgid.com/m/o/moti2.al.701831.js"
+              async
             ></script>
           </div>
         );
