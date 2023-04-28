82b9ed1874a75af031080c1f46a7c767f37546d3
Dumitru C <canteadumitru@gmail.com>
2023-04-28 13:22:30 +0300

    tsc fix

--- src/news/routes/rss-proxy/agerpres_ro.ts
+++ src/news/routes/rss-proxy/agerpres_ro.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import * as Rss from "rss";
 import { URL } from "url";
-import * as Xray from "x-ray";
+
+const Xray = require("x-ray");
 
 const USER_AGENT =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
--- src/weather/routes/redirect-handler.ts
+++ src/weather/routes/redirect-handler.ts
@@ -7,193 +7,251 @@ import { getLanguageFromQueryString } from "../../ournet/view-model";
 import { WidgetOldPlaceHandler } from "../handlers/widget-old-place-handler";
 import { parse as parseUrl } from "url";
 
-
 export class Widget2RedirectRouter extends OurnetRouter {
-    constructor() {
-        super('/widget2')
-    }
-    protected createHander(req: Request, res: Response) {
-        const input = this.formatInput(req, res);
-        const config = createAppConfig(input.project, input.country);
-        const links = sitemap(config.languages[0]);
-        const lang = getLanguageFromQueryString(config, input.url.query);
+  constructor() {
+    super("/widget2");
+  }
+  protected createHander(req: Request, res: Response) {
+    const input = this.formatInput(req, res);
+    const config = createAppConfig(input.project, input.country);
+    const links = sitemap(config.languages[0]);
+    const lang = getLanguageFromQueryString(config, input.url.query);
 
-        const handler = new RedirectHandler({ code: 301, location: links.weather.widget({ ul: lang }), req, res });
+    const handler = new RedirectHandler({
+      code: 301,
+      location: links.weather.widget({ ul: lang }),
+      req,
+      res
+    });
 
-        handler.setCacheControl(60 * 24);
+    handler.setCacheControl(60 * 24);
 
-        return handler;
-    }
+    return handler;
+  }
 }
 
-const ROUTE_PREFIX = '(prognoza|pogoda|vremea|forecast|tempo|pocasi|elorejelzes|mot)';
+const ROUTE_PREFIX =
+  "(prognoza|pogoda|vremea|forecast|tempo|pocasi|elorejelzes|mot)";
 
 interface PrefixRedirectRouterData {
-    prefix: string
+  prefix: string;
 }
 
 export class PrefixPlacesRedirectRouter extends OurnetRouter {
-    constructor() {
-        super(`/${ROUTE_PREFIX}/places`)
-    }
-    protected createHander(req: Request, res: Response) {
-        const input = this.formatInput(req, res);
-        const config = createAppConfig(input.project, input.country);
-        const links = sitemap(config.languages[0]);
-        const lang = getLanguageFromQueryString(config, input.url.query);
+  constructor() {
+    super(`/${ROUTE_PREFIX}/places`);
+  }
+  protected createHander(req: Request, res: Response) {
+    const input = this.formatInput(req, res);
+    const config = createAppConfig(input.project, input.country);
+    const links = sitemap(config.languages[0]);
+    const lang = getLanguageFromQueryString(config, input.url.query);
 
-        const handler = new RedirectHandler({ code: 301, location: links.weather.places({ ul: lang }), req, res });
+    const handler = new RedirectHandler({
+      code: 301,
+      location: links.weather.places({ ul: lang }),
+      req,
+      res
+    });
 
-        handler.setCacheControl(60 * 24);
+    handler.setCacheControl(60 * 24);
 
-        return handler;
-    }
+    return handler;
+  }
 }
 
 interface PrefixPlacesRedirectRouterData extends PrefixRedirectRouterData {
-    admin1Code: string
+  admin1Code: string;
 }
 
 export class PrefixPlacesAdminRedirectRouter extends OurnetRouter<PrefixPlacesRedirectRouterData> {
-    constructor() {
-        super(`/${ROUTE_PREFIX}/places/([^/]+)`, ['prefix', 'admin1Code'])
-    }
-    protected createHander(req: Request, res: Response, data: PrefixPlacesRedirectRouterData) {
-        const input = this.formatInput(req, res);
-        const config = createAppConfig(input.project, input.country);
-        const links = sitemap(config.languages[0]);
-        const lang = getLanguageFromQueryString(config, input.url.query);
+  constructor() {
+    super(`/${ROUTE_PREFIX}/places/([^/]+)`, ["prefix", "admin1Code"]);
+  }
+  protected createHander(
+    req: Request,
+    res: Response,
+    data: PrefixPlacesRedirectRouterData
+  ) {
+    const input = this.formatInput(req, res);
+    const config = createAppConfig(input.project, input.country);
+    const links = sitemap(config.languages[0]);
+    const lang = getLanguageFromQueryString(config, input.url.query);
 
-        const handler = new RedirectHandler({ code: 301, location: links.weather.places.byAdm1(data.admin1Code, { ul: lang }), req, res });
+    const handler = new RedirectHandler({
+      code: 301,
+      location: links.weather.places.byAdm1(data.admin1Code, { ul: lang }),
+      req,
+      res
+    });
 
-        handler.setCacheControl(60 * 24);
+    handler.setCacheControl(60 * 24);
 
-        return handler;
-    }
+    return handler;
+  }
 }
 
 interface PrefixPlaceRedirectRouterData extends PrefixRedirectRouterData {
-    placeId: string
+  placeId: string;
 }
 
 export class PrefixPlaceRedirectRouter extends OurnetRouter<PrefixPlaceRedirectRouterData> {
-    constructor() {
-        super(`/${ROUTE_PREFIX}/(\\d+)`, ['prefix', 'placeId'])
-    }
-    protected createHander(req: Request, res: Response, data: PrefixPlaceRedirectRouterData) {
-        const input = this.formatInput(req, res);
-        const config = createAppConfig(input.project, input.country);
-        const links = sitemap(config.languages[0]);
-        const lang = getLanguageFromQueryString(config, input.url.query);
+  constructor() {
+    super(`/${ROUTE_PREFIX}/(\\d+)`, ["prefix", "placeId"]);
+  }
+  protected createHander(
+    req: Request,
+    res: Response,
+    data: PrefixPlaceRedirectRouterData
+  ) {
+    const input = this.formatInput(req, res);
+    const config = createAppConfig(input.project, input.country);
+    const links = sitemap(config.languages[0]);
+    const lang = getLanguageFromQueryString(config, input.url.query);
 
-        const handler = new RedirectHandler({ code: 301, location: links.weather.place(data.placeId, { ul: lang }), req, res });
+    const handler = new RedirectHandler({
+      code: 301,
+      location: links.weather.place(data.placeId, { ul: lang }),
+      req,
+      res
+    });
 
-        handler.setCacheControl(60 * 24);
+    handler.setCacheControl(60 * 24);
 
-        return handler;
-    }
+    return handler;
+  }
 }
 export class PrefixPlace10DaysRedirectRouter extends OurnetRouter<PrefixPlaceRedirectRouterData> {
-    constructor() {
-        super(`/${ROUTE_PREFIX}/(\\d+)/\\w+`, ['prefix', 'placeId'])
-    }
-    protected createHander(req: Request, res: Response, data: PrefixPlaceRedirectRouterData) {
-        const input = this.formatInput(req, res);
-        const config = createAppConfig(input.project, input.country);
-        const links = sitemap(config.languages[0]);
-        const lang = getLanguageFromQueryString(config, input.url.query);
+  constructor() {
+    super(`/${ROUTE_PREFIX}/(\\d+)/\\w+`, ["prefix", "placeId"]);
+  }
+  protected createHander(
+    req: Request,
+    res: Response,
+    data: PrefixPlaceRedirectRouterData
+  ) {
+    const input = this.formatInput(req, res);
+    const config = createAppConfig(input.project, input.country);
+    const links = sitemap(config.languages[0]);
+    const lang = getLanguageFromQueryString(config, input.url.query);
 
-        const handler = new RedirectHandler({ code: 301, location: links.weather.place(data.placeId, { ul: lang }), req, res });
+    const handler = new RedirectHandler({
+      code: 301,
+      location: links.weather.place(data.placeId, { ul: lang }),
+      req,
+      res
+    });
 
-        handler.setCacheControl(60 * 24);
+    handler.setCacheControl(60 * 24);
 
-        return handler;
-    }
+    return handler;
+  }
 }
 
 export class PrefixPlaceNameRedirectRouter extends OurnetRouter<PrefixPlaceRedirectRouterData> {
-    constructor() {
-        super(`/vremea/ro/([^/]+)/(\\d+)`, ['prefix', 'placeId'])
-    }
-    protected createHander(req: Request, res: Response, data: PrefixPlaceRedirectRouterData) {
-        const input = this.formatInput(req, res);
-        const config = createAppConfig(input.project, input.country);
-        const links = sitemap(config.languages[0]);
-        const lang = getLanguageFromQueryString(config, input.url.query);
+  constructor() {
+    super(`/vremea/ro/([^/]+)/(\\d+)`, ["prefix", "placeId"]);
+  }
+  protected createHander(
+    req: Request,
+    res: Response,
+    data: PrefixPlaceRedirectRouterData
+  ) {
+    const input = this.formatInput(req, res);
+    const config = createAppConfig(input.project, input.country);
+    const links = sitemap(config.languages[0]);
+    const lang = getLanguageFromQueryString(config, input.url.query);
 
-        const handler = new RedirectHandler({ code: 301, location: links.weather.place(data.placeId, { ul: lang }), req, res });
+    const handler = new RedirectHandler({
+      code: 301,
+      location: links.weather.place(data.placeId, { ul: lang }),
+      req,
+      res
+    });
 
-        handler.setCacheControl(60 * 24);
+    handler.setCacheControl(60 * 24);
 
-        return handler;
-    }
+    return handler;
+  }
 }
 
 export class Place10DaysRedirectRouter extends OurnetRouter<PrefixPlaceRedirectRouterData> {
-    constructor() {
-        super(`/(\\d+)/\\w+`, ['placeId'])
-    }
-    protected createHander(req: Request, res: Response, data: PrefixPlaceRedirectRouterData) {
-        const input = this.formatInput(req, res);
-        const config = createAppConfig(input.project, input.country);
-        const links = sitemap(config.languages[0]);
-        const lang = getLanguageFromQueryString(config, input.url.query);
+  constructor() {
+    super(`/(\\d+)/\\w+`, ["placeId"]);
+  }
+  protected createHander(
+    req: Request,
+    res: Response,
+    data: PrefixPlaceRedirectRouterData
+  ) {
+    const input = this.formatInput(req, res);
+    const config = createAppConfig(input.project, input.country);
+    const links = sitemap(config.languages[0]);
+    const lang = getLanguageFromQueryString(config, input.url.query);
 
-        const handler = new RedirectHandler({ code: 301, location: links.weather.place(data.placeId, { ul: lang }), req, res });
+    const handler = new RedirectHandler({
+      code: 301,
+      location: links.weather.place(data.placeId, { ul: lang }),
+      req,
+      res
+    });
 
-        handler.setCacheControl(60 * 24);
+    handler.setCacheControl(60 * 24);
 
-        return handler;
-    }
+    return handler;
+  }
 }
 
 export class OldWidgetRedirectRouter extends OurnetRouter {
-    constructor() {
-        super(`/(widget|resources)/widgetframe`)
+  constructor() {
+    super(`/(widget|resources)/widgetframe`);
+  }
+  protected createHander(req: Request, res: Response) {
+    const input = this.formatInput(req, res);
+
+    if (!input.url.query.id) {
+      input.url = parseUrl((input.req.url || "").replace(/&amp;/g, "&"), true);
     }
-    protected createHander(req: Request, res: Response) {
-        const input = this.formatInput(req, res);
 
-        if (!input.url.query.id) {
-            input.url = parseUrl((input.req.url || '').replace(/&amp;/g, '&'), true);
-        }
+    const handler = new WidgetOldPlaceHandler({
+      country: input.country,
+      host: input.host,
+      id: parseInt(input.url.query.id as string),
+      project: input.project,
+      url: input.url,
+      req,
+      res
+    });
 
-        const handler = new WidgetOldPlaceHandler({
-            country: input.country,
-            host: input.host,
-            id: parseInt(input.url.query.id as string),
-            project: input.project,
-            url: input.url, req, res
-        });
+    handler.setCacheControl(60 * 24);
 
-        handler.setCacheControl(60 * 24);
-
-        return handler;
-    }
+    return handler;
+  }
 }
 
 export class PrefixOldWidgetRedirectRouter extends OurnetRouter {
-    constructor() {
-        super(`/${ROUTE_PREFIX}/widget/widgetframe`)
+  constructor() {
+    super(`/${ROUTE_PREFIX}/widget/widgetframe`);
+  }
+  protected createHander(req: Request, res: Response) {
+    const input = this.formatInput(req, res);
+
+    if (!input.url.query.id) {
+      input.url = parseUrl((input.req.url || "").replace(/&amp;/g, "&"), true);
     }
-    protected createHander(req: Request, res: Response) {
-        const input = this.formatInput(req, res);
 
-        if (!input.url.query.id) {
-            input.url = parseUrl((input.req.url || '').replace(/&amp;/g, '&'), true);
-        }
+    const handler = new WidgetOldPlaceHandler({
+      country: input.country,
+      host: input.host,
+      id: parseInt(input.url.query.id as string),
+      project: input.project,
+      url: input.url,
+      req,
+      res
+    });
 
-        const handler = new WidgetOldPlaceHandler({
-            country: input.country,
-            host: input.host,
-            id: parseInt(input.url.query.id as string),
-            project: input.project,
-            url: input.url, req, res
-        });
+    handler.setCacheControl(60 * 24);
 
-        handler.setCacheControl(60 * 24);
-
-        return handler;
-    }
+    return handler;
+  }
 }
-
