1cc7bee1b4eacf824bedcfea0a7cd14f68aec956
Dumitru C <canteadumitru@gmail.com>
2023-10-03 10:41:06 +0300

    lazy ads

--- src/horoscope/views/components/ads/ad-bottom.tsx
+++ src/horoscope/views/components/ads/ad-bottom.tsx
@@ -1,11 +1,20 @@
-
-import * as React from 'react';
+import * as React from "react";
 
 export function AdBottom() {
-    return (
-        <div>
-            <ins className='adsbygoogle' style={{ display: 'block' }} data-ad-client='ca-pub-3959589883092051' data-ad-slot='1115823833' data-ad-format='auto'></ins>
-            <script dangerouslySetInnerHTML={{ __html: '(adsbygoogle = window.adsbygoogle || []).push({});' }}></script>
-        </div>
-    )
+  return (
+    <div>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-3959589883092051"
+        data-ad-slot="1115823833"
+        data-ad-format="auto"
+      ></ins>
+      {/* <script
+        dangerouslySetInnerHTML={{
+          __html: "(adsbygoogle = window.adsbygoogle || []).push({});"
+        }}
+      ></script> */}
+    </div>
+  );
 }
--- src/news/views/components/ads/ad-center.tsx
+++ src/news/views/components/ads/ad-center.tsx
@@ -1,11 +1,20 @@
-
-import * as React from 'react';
+import * as React from "react";
 
 export function AdCenter() {
-    return (
-        <div className="c-ad c-ad--wide">
-            <ins style={{display: 'block'}} data-ad-client="ca-pub-3959589883092051" data-ad-slot="8122115033" data-ad-format="auto" className="adsbygoogle"></ins>
-            <script dangerouslySetInnerHTML={{ __html: '(adsbygoogle = window.adsbygoogle || []).push({});' }}></script>
-        </div>
-    )
+  return (
+    <div className="c-ad c-ad--wide">
+      <ins
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-3959589883092051"
+        data-ad-slot="8122115033"
+        data-ad-format="auto"
+        className="adsbygoogle"
+      ></ins>
+      {/* <script
+        dangerouslySetInnerHTML={{
+          __html: "(adsbygoogle = window.adsbygoogle || []).push({});"
+        }}
+      ></script> */}
+    </div>
+  );
 }
