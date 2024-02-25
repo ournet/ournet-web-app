f3c8b468ded745ee3df47c2da71bf001c043ee66
Dumitru C <canteadumitru@gmail.com>
2024-02-25 15:20:33 +0200

    Revert "Update place-daily-report.tsx"
    This reverts commit e192156d0b994b85de3153b7c10d5ce3982dca17.

--- src/weather/views/components/forecast/place-daily-report.tsx
+++ src/weather/views/components/forecast/place-daily-report.tsx
@@ -79,7 +79,7 @@ export function PlaceDailyReport({
               style={{
                 border: "none",
                 overflow: "hidden",
-                aspectRatio: "5 / 3"
+                aspectRatio: "4 / 3"
               }}
             ></iframe>
           );
@@ -111,7 +111,7 @@ export function PlaceDailyReport({
               style={{
                 border: "none",
                 overflow: "hidden",
-                aspectRatio: "5 / 3"
+                aspectRatio: "4 / 3"
               }}
             ></iframe>
           );
